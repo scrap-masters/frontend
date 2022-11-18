66c27e2cd77ecadeb6b98bfe362174b9152c4a13
JakubZakowicz <j.zakowicz99@gmail.com>
2022-11-18 20:43:18 +0100

    #12 - format App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { BrowserRouter as Router } from "react-router-dom"
 import { Header } from "./components/Header/"
 import { Footer } from "./components/Footer"
-import { BrowserRouter as Router } from "react-router-dom"
 
 export const App = () => {
   return (
