293f7e7702597662ebab08644d5e89fc1a725998
JakubZakowicz <j.zakowicz99@gmail.com>
2022-11-18 16:40:54 +0100

    #11 - after review

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Footer from "./components/Footer"
+import { Footer } from "./components/Footer"
 
 export const App = () => {
   return (
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,9 @@
-const Footer = () => {
+export const Footer = () => {
   return (
-    <div className="bg-black py-16 text-white">
+    <footer className="bg-black py-16 text-white">
       <p className="text-center">
         Copyright @2022, Scrap masters. All rights reserved.
       </p>
-    </div>
+    </footer>
   )
 }
-
-export default Footer
