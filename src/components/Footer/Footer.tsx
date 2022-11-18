36b410b28d5e16318545ecad3db1447572cacfc7
Jakub Zakowicz <63516552+JakubZakowicz@users.noreply.github.com>
2022-11-18 18:45:01 +0100

    #11 - add footer (#18)

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-black py-16 text-white">
+      <p className="text-center">
+        Copyright @2022, Scrap masters. All rights reserved.
+      </p>
+    </footer>
+  )
+}
