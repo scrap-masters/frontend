cbb38410c9f5f80574b8c255051a0f15a2305926
JakubZakowicz <j.zakowicz99@gmail.com>
2022-11-19 22:05:25 +0100

    #15 - add tests to card components

--- src/components/CardImage/CardImage.tsx
+++ src/components/CardImage/CardImage.tsx
@@ -14,7 +14,11 @@ export const CardImage = (props: CardImageProps) => {
         className="m-10 grid h-[300px] w-[350px] rounded-2xl 
         text-4xl font-medium text-white"
       >
-        <img src={`/images/${imgUrl}`} alt="" className="col-end-1 row-end-1" />
+        <img
+          src={`/images/${imgUrl}`}
+          alt={title}
+          className="col-end-1 row-end-1"
+        />
         <div className="col-end-1 row-end-1 py-8 px-8 text-center">
           <p className="">{title}</p>
         </div>
