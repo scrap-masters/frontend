a6b5fc88ab6866aa88ef2dcea7145aaa3a332843
shiragaira <kacper.frankowski.5@gmail.com>
2022-11-20 14:09:03 +0100

    #13 - Global layout and test router wrapper

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,15 +1,9 @@
-import "tests.utils"
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "../../tests/tests.utils"
 import { Header } from "./Header"
-import { BrowserRouter as Router } from "react-router-dom"
 
 describe("<Header /> component", () => {
   it("render with proper text", () => {
-    render(
-      <Router>
-        <Header />
-      </Router>
-    )
+    render(<Header />)
     expect(screen.getByRole("heading", { level: 1 })).toBeInTheDocument()
   })
 })
