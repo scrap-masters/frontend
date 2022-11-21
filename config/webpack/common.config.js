ae6d92ff4a3b1f38416ba9c9d46e05e231ab1fa0
Kacper Frankowski <66391990+shiragaira@users.noreply.github.com>
2022-11-21 19:54:09 +0100

    #13 - Webpack image import configuration (#30)

--- config/webpack/common.config.js
+++ config/webpack/common.config.js
@@ -1,10 +1,11 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin"
 
 module.exports = {
   entry: "/index.tsx",
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".css"],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".css"]
   },
   context: path.resolve(__dirname, "../../src"),
   module: {
@@ -12,30 +13,39 @@ module.exports = {
       {
         test: /\.(js|ts)x?$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ["babel-loader"]
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader"]
       },
       {
         test: /\.(png|jpe?g|svg)$/,
         loader: "file-loader",
         options: {
           name: "[contenthash].[ext]",
-          outputPath: "assets",
-        },
+          outputPath: "assets"
+        }
       },
       {
         test: /\.(png|jpe?g|svg)$/,
         loader: "image-webpack-loader",
         options: {
-          bypassOnDebug: true,
-          optimizationLevel: 7,
-        },
-      },
-    ],
+          optipng: {
+            enabled: false
+          }
+        }
+      }
+    ]
   },
-  plugins: [new HtmlWebpackPlugin({ template: "index.html" })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: "index.html" }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: path.resolve(__dirname, "../../src/**/*.{ts,tsx,js,jsx}")
+      }
+    })
+  ]
 }
