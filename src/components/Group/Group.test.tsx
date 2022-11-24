f83342ca4cce5964adbca5154deb630dcef28417
JakubZakowicz <j.zakowicz99@gmail.com>
2022-11-24 20:37:54 +0100

    #14 - add more cases to Group.test.tsx

--- src/components/Group/Group.test.tsx
+++ src/components/Group/Group.test.tsx
@@ -3,12 +3,43 @@ import { render, screen } from "../../tests/tests.utils"
 import { Group } from "./Group"
 
 describe("<Group /> component", () => {
+  const title = "Wydziały"
+  const elements = [
+    { id: 1, name: "Informatyka" },
+    { id: 2, name: "Grafika" },
+    { id: 3, name: "Sieci" }
+  ]
+
   it("render with proper text", () => {
-    const elements = [{ id: 1, name: "Informatyka" }]
     render(
-      <Group title="Wydziały" route={routes.departments} elements={elements} />
+      <Group title={title} route={routes.departments} elements={elements} />
     )
     expect(screen.getByRole("heading", { level: 2 })).toBeInTheDocument()
-    expect(screen.getByText(elements[0].name)).toBeInTheDocument()
+  })
+
+  it("render without elements", () => {
+    render(<Group title={title} route={routes.departments} elements={[]} />)
+    const currentElements = document.querySelectorAll("p")
+    expect(currentElements).toHaveLength(0)
+  })
+
+  it("render with one Element", () => {
+    render(
+      <Group
+        title={title}
+        route={routes.departments}
+        elements={elements.slice(0, 1)}
+      />
+    )
+    const currentElements = document.querySelectorAll("p")
+    expect(currentElements).toHaveLength(1)
+  })
+
+  it("render with several elements", () => {
+    render(
+      <Group title={title} route={routes.departments} elements={elements} />
+    )
+    const currentElements = document.querySelectorAll("p")
+    expect(currentElements).toHaveLength(elements.length)
   })
 })
