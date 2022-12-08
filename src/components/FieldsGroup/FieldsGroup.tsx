0b2ad1c04432217f65a5120e1c006cb64e7b2156
JakubZakowicz <j.zakowicz99@gmail.com>
2022-12-08 20:00:56 +0100

    #35 - add GroupGridWrapper layout

--- src/components/FieldsGroup/FieldsGroup.tsx
+++ src/components/FieldsGroup/FieldsGroup.tsx
@@ -1,7 +1,7 @@
-import classNames from "classnames"
 import { colors } from "../../constants/colors"
 import { Card } from "../Card"
 import { Link } from "react-router-dom"
+import { GroupGridWrapper } from "../GroupGridWrapper"
 
 interface FieldsGroupProps {
   title: string
@@ -13,29 +13,19 @@ export const FieldsGroup = (props: FieldsGroupProps) => {
   const { title, fields, route } = props
 
   return (
-    <div className="px-20 py-10">
-      <h2 className="text-center text-[45px] font-semibold">{title}</h2>
-      <div
-        className={classNames(
-          "my-12",
-          fields?.length < 5
-            ? "flex flex-wrap justify-center gap-20"
-            : "grid grid-cols-[repeat(auto-fill,_350px)] place-content-around place-items-center gap-20"
-        )}
-      >
-        {fields.map(
-          ({ id, name, year, isFullTime }, index) =>
-            isFullTime && (
-              <Link key={id} to={route(id)}>
-                <Card
-                  id={id}
-                  name={`${name} - ${year} rok`}
-                  className={colors[index % colors.length]}
-                />
-              </Link>
-            )
-        )}
-      </div>
-    </div>
+    <GroupGridWrapper title={title} length={fields.length}>
+      {fields.map(
+        ({ id, name, year, isFullTime }, index) =>
+          isFullTime && (
+            <Link key={id} to={route(id)}>
+              <Card
+                id={id}
+                name={`${name} - ${year} rok`}
+                className={colors[index % colors.length]}
+              />
+            </Link>
+          )
+      )}
+    </GroupGridWrapper>
   )
 }
--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -1,7 +1,7 @@
-import classNames from "classnames"
 import { colors } from "../../constants/colors"
 import { Card, CardProps } from "../Card"
 import { Link } from "react-router-dom"
+import { GroupGridWrapper } from "../GroupGridWrapper"
 
 interface GroupProps {
   title: string
@@ -13,26 +13,12 @@ export const Group = (props: GroupProps) => {
   const { title, elements, route } = props
 
   return (
-    <div className="px-20 py-10">
-      <h2 className="text-center text-[45px] font-semibold">{title}</h2>
-      <div
-        className={classNames(
-          "my-12",
-          elements?.length < 5
-            ? "flex flex-wrap justify-center gap-20"
-            : "grid grid-cols-[repeat(auto-fill,_350px)] place-content-around place-items-center gap-20"
-        )}
-      >
-        {elements.map(({ id, name }, index) => (
-          <Link key={id} to={route(id)}>
-            <Card
-              id={id}
-              name={name}
-              className={colors[index % colors.length]}
-            />
-          </Link>
-        ))}
-      </div>
-    </div>
+    <GroupGridWrapper title={title} length={elements.length}>
+      {elements.map(({ id, name }, index) => (
+        <Link key={id} to={route(id)}>
+          <Card id={id} name={name} className={colors[index]} />
+        </Link>
+      ))}
+    </GroupGridWrapper>
   )
 }
