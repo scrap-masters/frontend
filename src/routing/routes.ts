eda54502847ca8009d2c19cb2d36673837c589d1
JakubZakowicz <j.zakowicz99@gmail.com>
2022-12-13 12:06:45 +0100

    #35 - resolve conflicts

--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -4,7 +4,5 @@ export const routes = {
   fields: (id?: number) =>
     id ? `/faculties/${id}/fields` : `/faculties/:id/fields`,
   specializations: (id: number) => `/fields/${id}/specializations`,
-  departments: (id: number) => `/departments/${id}/fields`,
-  fieldOfStudy: (id: number) => `/fields/${id}`,
   calendar: (id?: number) => (id ? `/calendar/${id}` : "/calendar/:id")
 }
