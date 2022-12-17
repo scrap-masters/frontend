7a8169a90096409db734051aeaa9831117b0e45b
JakubZakowicz <j.zakowicz99@gmail.com>
2022-12-17 15:52:01 +0100

    #34 - add error component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,13 +2,14 @@ import { Group } from "../../components/Group"
 import { useGetFaculties } from "../../api/faculties"
 import { routes } from "../../routing/routes"
 import { Loader } from "../../components/Loader"
+import { Error } from "../../components/Error"
 
 export const HomePage = () => {
-  const { data: facultiesData, isLoading, isError, error } = useGetFaculties()
+  const { data: facultiesData, isLoading, isError } = useGetFaculties()
 
   if (isLoading) return <Loader />
 
-  if (isError) return <p>{error.message}</p>
+  if (isError) return <Error />
 
   return (
     <Group
