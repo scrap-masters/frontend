3e4e8e6da5d7e2a4787b089541263829113fa1bc
Jakub Zakowicz <63516552+JakubZakowicz@users.noreply.github.com>
2022-12-17 21:16:08 +0100

    #34 - error handling (#56)

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,7 @@
+export const Error = () => {
+  return (
+    <div className="flex h-[400px] items-center justify-center">
+      <p className="text-3xl text-red-500">Coś poszło nie tak!</p>
+    </div>
+  )
+}
