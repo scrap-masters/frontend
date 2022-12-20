25d15fbd787c223b0759a6fdd2112149e4cb199d
JakubZakowicz <j.zakowicz99@gmail.com>
2022-12-20 11:13:00 +0100

    #52 - add calendar to lecturer plan

--- src/pages/LecturerCalendarPage/LecturerCalendarPage.tsx
+++ src/pages/LecturerCalendarPage/LecturerCalendarPage.tsx
@@ -1,3 +1,27 @@
+import { useMemo } from "react"
+import { useParams } from "react-router-dom"
+import { useGetLecturersPlan } from "../../api/lecturers"
+import { Calendar } from "../../components/Calendar"
+
 export const LecturerCalendarPage = () => {
-  return <div>LecturerCalendarPage</div>
+  const { name } = useParams()
+  const { data } = useGetLecturersPlan(name)
+  console.log(data)
+
+  const timetable = useMemo(() => {
+    return data?.data.timetable.map((event) => ({
+      start: event.start,
+      end: event.end,
+      title: event.title,
+      extendedProps: {
+        ...event
+      }
+    }))
+  }, [data])
+
+  return (
+    <div>
+      <Calendar timetable={timetable} />
+    </div>
+  )
 }
