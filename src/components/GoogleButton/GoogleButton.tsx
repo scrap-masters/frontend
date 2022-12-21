9b7d12d47692fde8647e0957fe864f684d02e3b9
JakubZakowicz <j.zakowicz99@gmail.com>
2022-12-21 15:31:09 +0100

    #58 - add exporting to csv file

--- src/components/GoogleButton/GoogleButton.tsx
+++ src/components/GoogleButton/GoogleButton.tsx
@@ -1,9 +1,6 @@
 import { EventInput } from "@fullcalendar/react"
 import moment from "moment"
-import ICalendarLink from "react-icalendar-link"
-import { useState } from "react"
-const ics = require("ics")
-import ICalParser from 'ical-js-parser';
+import { CSVLink } from "react-csv"
 
 export interface GoogleButtonProps {
   timetable: Array<EventInput>
@@ -11,56 +8,46 @@ export interface GoogleButtonProps {
 
 export const GoogleButton = (props: GoogleButtonProps) => {
   const { timetable } = props
-  const [text, setText] = useState(null)
+
+  const events = [
+    [
+      "Subject",
+      "Start Date",
+      "Start Time",
+      "End Date",
+      "End Time",
+      "All Day Event",
+      "Description",
+      "Location",
+      "Private"
+    ]
+  ]
 
   // console.log(timetable)
+  timetable.forEach((data) => {
+    const event = data.extendedProps
+    const startDate = moment(event.start).format("DD/MM/YYYY")
+    const startTime = moment(event.start).format("hh:mm:ss a")
+    const endDate = moment(event.end).format("DD/MM/YYYY")
+    const endTime = moment(event.end).format("hh:mm:ss a")
+    events.push([
+      event.title,
+      startDate,
+      startTime,
+      endDate,
+      endTime,
+      "FALSE",
+      event.lecturer,
+      event.room,
+      "FALSE"
+    ])
+  })
 
-  const exportToICS = () => {
-    const events = []
-    timetable.forEach((data) => {
-      const event = data.extendedProps
-
-      const start = moment(event.start)
-        .format("YYYY-M-D-H-m")
-        .split("-")
-        .map(Number)
-
-      const end = moment(event.end)
-        .format("YYYY-M-D-H-m")
-        .split("-")
-        .map(Number)
-
-      const newEvent = {
-        title: event.title,
-        start: start,
-        duration: { hours: 1, minutes: 30 }
-      }
-
-      events.push(newEvent)
-    })
-
-    console.log(ics.createEvents(events))
-    // setText(ics.createEvents(events))
-    // const result = ICalParser.toString(events)
-    console.log(result)
-  }
-
-  console.log(text)
-
-  const events = {
-    title: "My Title",
-    description: "My Description",
-    startTime: "2022-12-20T10:30:00+10:00",
-    location: "10 Carlotta St, Artarmon NSW 2064, Australia"
-  }
+  console.log(events)
 
   return (
     <div>
-      <button className="bg-gray-500" onClick={exportToICS}>
-        Export to ICS
-      </button>
-      <p>{text?.value}</p>
-      <ICalendarLink rawContent={text?.value}>Add to Calendar</ICalendarLink>
+      <CSVLink data={events}>Download me</CSVLink>;
     </div>
   )
 }
