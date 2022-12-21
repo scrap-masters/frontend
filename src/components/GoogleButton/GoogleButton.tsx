2b4a3b5d5fe1dc66d2f997cfb512df8e903713c2
JakubZakowicz <j.zakowicz99@gmail.com>
2022-12-21 12:18:24 +0100

    #58 - export event using gapi-script

--- src/components/GoogleButton/GoogleButton.tsx
+++ src/components/GoogleButton/GoogleButton.tsx
@@ -1,36 +1,78 @@
 import { EventInput } from "@fullcalendar/react"
-import ApiCalendar from "react-google-calendar-api"
+import { gapi, loadAuth2 } from "gapi-script"
+import { useEffect, useState } from "react"
 
 export interface GoogleButtonProps {
   timetable: Array<EventInput>
 }
 
 export const GoogleButton = (props: GoogleButtonProps) => {
+  const [user, setUser] = useState(null)
   const { timetable } = props
 
-  console.log(timetable)
+  useEffect(() => {
+    const setAuth2 = async () => {
+      const auth2 = await loadAuth2(gapi, process.env.CLIENT_ID, "")
+      if (auth2.isSignedIn.get()) {
+        updateUser(auth2.currentUser.get())
+      } else {
+        attachSignin(document.getElementById("button"), auth2)
+      }
+    }
+    setAuth2()
+  }, [])
 
-  const config = {
-    clientId: process.env.CLIENT_ID,
-    apiKey: process.env.API_KEY,
-    accessToken: process.env.ACCESS_TOKEN,
-    scope: "https://www.googleapis.com/auth/calendar",
-    discoveryDocs: [
-      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
-    ]
+  useEffect(() => {
+    if (!user) {
+      const setAuth2 = async () => {
+        const auth2 = await loadAuth2(gapi, process.env.REACT_APP_CLIENT_ID, "")
+        attachSignin(document.getElementById("button"), auth2)
+      }
+      setAuth2()
+    }
+  }, [user])
+
+  const updateUser = (currentUser) => {
+    const name = currentUser.getBasicProfile().getName()
+    const accessToken = currentUser.xc.access_token
+    const calendarID = currentUser.getBasicProfile().getEmail()
+    setUser({
+      name,
+      calendarID,
+      accessToken
+    })
   }
 
-  const apiCalendar = new ApiCalendar(config)
-
-  const handleClick = () => {
-    apiCalendar.handleAuthClick()
+  const attachSignin = (element, auth2) => {
+    auth2.attachClickHandler(
+      element,
+      {},
+      (googleUser) => {
+        console.log(googleUser)
+      },
+      (error) => {
+        console.log(JSON.stringify(error))
+      }
+    )
   }
 
   const exportEvents = () => {
     const { timeZone } = Intl.DateTimeFormat().resolvedOptions()
-    timetable.forEach((data) => {
-      const event = data.extendedProps
-      const newEvent = {
+    const event = timetable[0].extendedProps
+    const newEvent = [
+      {
+        summary: event.title,
+        location: event.room,
+        start: {
+          timeZone,
+          dateTime: event.start
+        },
+        end: {
+          timeZone,
+          dateTime: event.end
+        }
+      },
+      {
         summary: event.title,
         location: event.room,
         start: {
@@ -42,15 +84,37 @@ export const GoogleButton = (props: GoogleButtonProps) => {
           dateTime: event.end
         }
       }
-      apiCalendar.createEvent(newEvent).then((res) => console.log(res))
-    })
+    ]
+    const initiate = () => {
+      gapi.client
+        .request({
+          path: `https://www.googleapis.com/calendar/v3/calendars/${user.calendarID}/events`,
+          method: "POST",
+          body: newEvent,
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${user.accessToken}`
+          }
+        })
+        .then(
+          (response) => {
+            return [true, response]
+          },
+          function (err) {
+            console.log(err)
+            return [false, err]
+          }
+        )
+    }
+    gapi.load("client", initiate)
   }
 
   return (
     <div>
-      <button className="bg-blue-500" onClick={handleClick}>
+      <button className="bg-blue-500" id="button">
         Login
       </button>
+      {user && <p>{user.name}</p>}
       <button className="ml-2 bg-blue-500" onClick={exportEvents}>
         export
       </button>
